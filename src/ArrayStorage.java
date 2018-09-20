8ec402501fd9565d861d24a2705cf4890c644e25
sashalik <sashal1912@gmail.com>
2018-09-20 23:29:23 +0700

    Moved the execution condition check. Added " ArrayStorage.getIndex (Resume resume)". Redefined the 'Equals' method for the 'Resume'class.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,68 +4,89 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    Resume[] storage = new Resume[10000];
-    int size = 0;
+    private Resume[] storage = new Resume[10000];
+    private int size = 0;
 
-    void clear() {
+    public void clear() {
         Arrays.fill(storage, 0, size, null);
         size = 0;
     }
 
-    void save(Resume resume) {
-        int i = getIndex(resume.uuid);
-        if (i == -1) {
-            storage[size] = resume;
-            size++;
-        }
-    }
+    public void save(Resume resume) {
 
-    Resume get(String uuid) {
-        int i = getIndex(uuid);
-        if (i == -1) {
-            return null;
+        if (getIndex(resume) != -1) {
+            System.out.println("Resume '" + resume + "' is already in the array");
+        } else if (this.size() == 10000) {
+            System.out.println("The size of the array has reached acceptable limits");
         } else {
-            return storage[i];
+            storage[size++] = resume;
         }
     }
 
-    void delete(String uuid) {
-        int i = getIndex(uuid);
-        if (i != -1) {
-            storage[i] = null;
-            rebuildArray(i);
+    public Resume get(String uuid) {
+
+        int index = getIndex(uuid);
+
+        if (index == -1) {
+            System.out.println("Resume '" + uuid + "' is not contained in the array");
+            return null;
+        }
+        return storage[index];
+    }
+
+    public void delete(String uuid) {
+
+        int index = getIndex(uuid);
+
+        if (index == -1) {
+            System.out.println("Resume '" + uuid + "' is not contained in the array");
+        } else {
+            rebuildArray(index);
             size--;
         }
     }
 
-    void update(Resume resume, String newUuid){
-        int i = getIndex(resume.uuid);
-        if (i !=-1){
-            resume.uuid = newUuid;
-            storage[i] = resume;
+    public void update(Resume resume) {
+        int index = getIndex(resume);
+
+        if (index == -1) {
+            System.out.println("Resume '" + resume + "' is not contained in the array");
+        } else {
+            storage[index] = resume;
         }
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
-    Resume[] getAll() {
+    public Resume[] getAll() {
         return Arrays.copyOf(storage, size);
     }
 
-    int size() {
+    public int size() {
         return size;
     }
 
     // пересобираем массив.
-    void rebuildArray(int i) {
+    private void rebuildArray(int i) {
         System.arraycopy(storage, i + 1, storage, i, size - i);
     }
 
-    //Метод получения текущего индекса для объекта
-    int getIndex(String uuid) {
+    //Метод получения текущего индекса по uuid
+    private int getIndex(String uuid) {
+
         for (int i = 0; i < size; i++) {
             if (storage[i].uuid == uuid) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    // Метод получени индекса по поиску объекта в массиве
+    private int getIndex(Resume resume) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].equals(resume)) {
                 return i;
             }
         }
--- src/MainArray.java
+++ src/MainArray.java
@@ -13,7 +13,7 @@ public class MainArray {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         Resume r;
         while (true) {
-            System.out.print("Введите одну из команд - (list | save uuid | update uuid newuuid | delete uuid | get uuid | clear | exit): ");
+            System.out.print("Введите одну из команд - (list | save uuid | update uuid | delete uuid | get uuid | clear | exit): ");
             String[] params = reader.readLine().trim().toLowerCase().split(" ");
             if (params.length < 1 || params.length > 3) {
                 System.out.println("Неверная команда.");
@@ -36,60 +36,24 @@ public class MainArray {
                     System.out.println(ARRAY_STORAGE.size());
                     break;
                 case "save":
-                    if (ARRAY_STORAGE.getIndex(uuid) != -1) {
-                        System.out.println("Resume '" + uuid + "' is already in the array");
-                        printAll();
-                        break;
-                    } else if (ARRAY_STORAGE.size() == 10000) {
-                        System.out.println("The size of the array has reached acceptable limits");
-                        printAll();
-                        break;
-                    } else {
-                        r = new Resume();
-                        r.uuid = uuid;
-                        ARRAY_STORAGE.save(r);
-                        printAll();
-                        break;
-                    }
+                    r = new Resume();
+                    r.uuid = uuid;
+                    ARRAY_STORAGE.save(r);
+                    printAll();
+                    break;
                 case "update":
-                    if (ARRAY_STORAGE.getIndex(uuid) == -1) {
-                        System.out.println("Resume '" + uuid + "' is not contained in the array");
-                        printAll();
-                        break;
-                    } else if (newUuid == null) {
-                        System.out.println("No new value specified for the change");
-                        printAll();
-                        break;
-                    } else if (ARRAY_STORAGE.getIndex(newUuid) != -1) {
-                        System.out.println("The Resume '" + uuid + "' cannot be updated. The array already has an element with '" + newUuid + "'.");
-                        printAll();
-                        break;
-                    } else {
-                        r = new Resume();
-                        r.uuid = uuid;
-                        ARRAY_STORAGE.update(r, newUuid);
-                        printAll();
-                        break;
-                    }
+                    r = new Resume();
+                    r.uuid = uuid;
+                    ARRAY_STORAGE.update(r);
+                    printAll();
+                    break;
                 case "delete":
-                    if (ARRAY_STORAGE.getIndex(uuid) == -1) {
-                        System.out.println("Resume '" + uuid + "' is not contained in the array");
-                        printAll();
-                        break;
-                    } else {
-                        ARRAY_STORAGE.delete(uuid);
-                        printAll();
-                        break;
-                    }
+                    ARRAY_STORAGE.delete(uuid);
+                    printAll();
+                    break;
                 case "get":
-                    if (ARRAY_STORAGE.getIndex(uuid) == -1) {
-                        System.out.println("Resume '" + uuid + "' is not contained in the array");
-                        printAll();
-                        break;
-                    } else {
-                        System.out.println(ARRAY_STORAGE.get(uuid));
-                        break;
-                    }
+                    System.out.println(ARRAY_STORAGE.get(uuid));
+                    break;
                 case "clear":
                     ARRAY_STORAGE.clear();
                     printAll();
