4c7d6def1b80020077c61ba563fbb397dbde9032
sashalik <sashal1912@gmail.com>
2018-09-21 22:02:08 +0700

    Added new constructor for class Resume. Removed method getIndex(String uuid).

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,7 +4,8 @@ import java.util.Arrays;
  * Array based storage for Resumes
  */
 public class ArrayStorage {
-    private Resume[] storage = new Resume[10000];
+    private static final int MAX_LENGTH = 10000;
+    private Resume[] storage = new Resume[MAX_LENGTH];
     private int size = 0;
 
     public void clear() {
@@ -13,10 +14,9 @@ public class ArrayStorage {
     }
 
     public void save(Resume resume) {
-
         if (getIndex(resume) != -1) {
             System.out.println("Resume '" + resume + "' is already in the array");
-        } else if (this.size() == 10000) {
+        } else if (size == MAX_LENGTH) {
             System.out.println("The size of the array has reached acceptable limits");
         } else {
             storage[size++] = resume;
@@ -24,8 +24,7 @@ public class ArrayStorage {
     }
 
     public Resume get(String uuid) {
-
-        int index = getIndex(uuid);
+        int index = getIndex(new Resume(uuid));
 
         if (index == -1) {
             System.out.println("Resume '" + uuid + "' is not contained in the array");
@@ -35,8 +34,7 @@ public class ArrayStorage {
     }
 
     public void delete(String uuid) {
-
-        int index = getIndex(uuid);
+        int index = getIndex(new Resume(uuid));
 
         if (index == -1) {
             System.out.println("Resume '" + uuid + "' is not contained in the array");
@@ -72,18 +70,7 @@ public class ArrayStorage {
         System.arraycopy(storage, i + 1, storage, i, size - i);
     }
 
-    //Метод получения текущего индекса по uuid
-    private int getIndex(String uuid) {
-
-        for (int i = 0; i < size; i++) {
-            if (storage[i].uuid == uuid) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    // Метод получени индекса по поиску объекта в массиве
+    // Метод получения индекса по поиску объекта в массиве
     private int getIndex(Resume resume) {
         for (int i = 0; i < size; i++) {
             if (storage[i].equals(resume)) {
