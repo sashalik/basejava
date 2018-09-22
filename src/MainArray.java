1ece95553345d574d8975446361cb86505c32c5e
sashalik <sashal1912@gmail.com>
2018-09-22 14:01:22 +0700

    Code optimization

--- src/MainArray.java
+++ src/MainArray.java
@@ -15,18 +15,13 @@ public class MainArray {
         while (true) {
             System.out.print("Введите одну из команд - (list | save uuid | update uuid | delete uuid | get uuid | clear | exit): ");
             String[] params = reader.readLine().trim().toLowerCase().split(" ");
-            if (params.length < 1 || params.length > 3) {
+            if (params.length < 1 || params.length > 2) {
                 System.out.println("Неверная команда.");
                 continue;
             }
             String uuid = null;
-            String newUuid = null;
             if (params.length == 2) {
                 uuid = params[1].intern();
-            }
-            if (params.length == 3) {
-                uuid = params[1].intern();
-                newUuid = params[2].intern();
             }
             switch (params[0]) {
                 case "list":
