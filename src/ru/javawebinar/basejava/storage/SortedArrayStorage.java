7738538ed6d5878ce704b79a515f2119a1d4294c
sashalik <sashal1912@gmail.com>
2018-09-24 01:28:39 +0700

    Implementation class SortedArrayStorage

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -7,17 +7,24 @@ import java.util.Arrays;
 public class SortedArrayStorage extends AbstractArrayStorage {
     @Override
     public void clear() {
-
+        Arrays.fill(storage, 0, size, null);
+        size = 0;
     }
 
     @Override
-    public void update(Resume r) {
+    public void update(Resume resume) {
+        int index = getIndex(resume.getUuid());
 
+        if (index < 0) {
+            System.out.println("Resume '" + resume + "' is not contained in the array");
+        } else {
+            storage[index] = resume;
+        }
     }
 
     @Override
     public void save(Resume resume) {
-        if (getIndex(resume.getUuid()) != -1) {
+        if (getIndex(resume.getUuid()) >= 0) {
             System.out.println("Resume '" + resume + "' is already in the array");
         } else if (size == STORAGE_LIMIT) {
             System.out.println("The size of the array has reached acceptable limits");
@@ -29,25 +36,31 @@ public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
     public void delete(String uuid) {
+        int index = getIndex(uuid);
 
+        if (index < 0) {
+            System.out.println("Resume '" + uuid + "' is not contained in the array");
+        } else {
+            rebuildArray(index);
+            size--;
+        }
     }
 
-    @Override
-    public Resume[] getAll() {
-        return super.getAll();
-    }
-
-    private void sortArray() {
-
+    void sortArray() {
         for (int i = size - 1; i > 0; i--) {
             for (int j = 0; j < i; j++) {
-                if (storage[j].hashCode() > storage[j + 1].hashCode()) {
+                if (storage[j].compareTo(storage[j + 1]) > 0) {
                     Resume tmp = storage[j];
                     storage[j] = storage[j + 1];
                     storage[j + 1] = tmp;
                 }
             }
         }
+    }
+
+    @Override
+    public Resume[] getAll() {
+        return super.getAll();
     }
 
     @Override
