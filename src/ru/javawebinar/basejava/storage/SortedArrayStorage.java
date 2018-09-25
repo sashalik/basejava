ff0fdb46d4b169ecb3753691b49072d8b6471406
VESTADOM\LaudenschliderAA <LaudenschliderAA@alfastrah.ru>
2018-09-25 16:51:30 +0700

    Optimization methods

--- src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
+++ src/ru/javawebinar/basejava/storage/SortedArrayStorage.java
@@ -7,48 +7,22 @@ import java.util.Arrays;
 public class SortedArrayStorage extends AbstractArrayStorage {
 
     @Override
-    public void save(Resume resume) {
-        int index = getIndex(resume.getUuid());
+    public void saveResume(Resume resume, int index) {
+        int indexBuf = -index - 1;
 
-        if (index >= 0) {
-            System.out.println("Resume '" + resume + "' is already in the array");
-        } else if (size == STORAGE_LIMIT) {
-            System.out.println("The size of the array has reached acceptable limits");
-        } else {
-            rebuildArray(-index - 1, "Save");
-            storage[-index - 1] = resume;
-            size++;
-        }
+        System.arraycopy(storage, indexBuf, storage, indexBuf + 1, size - indexBuf);
+        storage[indexBuf] = resume;
+        size++;
     }
 
     @Override
-    public void delete(String uuid) {
-        int index = getIndex(uuid);
-
-        if (index < 0) {
-            System.out.println("Resume '" + uuid + "' is not contained in the array");
-        } else {
-            rebuildArray(index, "Del");
-            size--;
-        }
+    public void deleteResume(int index) {
+        System.arraycopy(storage, index + 1, storage, index, size - index - 1);
+        size--;
     }
-
-    // пересобираем массив.
-    private void rebuildArray(int i, String action) {
-        if (action.equals("Save")) {
-            System.arraycopy(storage, i, storage, i + 1, size - i);
-        }
-
-        if (action.equals("Del")) {
-            System.arraycopy(storage, i + 1, storage, i, size - i-1);
-        }
-    }
-
 
     @Override
     protected int getIndex(String uuid) {
-        Resume searchKey = new Resume();
-        searchKey.setUuid(uuid);
-        return Arrays.binarySearch(storage, 0, size, searchKey);
+        return Arrays.binarySearch(storage, 0, size, new Resume(uuid));
     }
 }
