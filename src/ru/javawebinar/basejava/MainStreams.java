f715289cf55b1012bdea75371f2d592fb106909d
sashalik <sashal1912@gmail.com>
2019-01-09 07:14:19 +0700

    Refactoring HW12

--- src/ru/javawebinar/basejava/MainStreams.java
+++ src/ru/javawebinar/basejava/MainStreams.java
@@ -7,7 +7,7 @@ import java.util.stream.IntStream;
 
 public class MainStreams {
     public static void main(String[] args) {
-        List<Integer> integerList = Arrays.asList(1, 2, 3, 4, 2, 5);
+        List<Integer> integerList = Arrays.asList(1, 2, 3, 3, 2, 5);
         int[] values = {9, 8};
         // {1, 2, 3, 3, 2, 3};       Тестовые наборы
         // {1, 9, 2, 3, 5, 7, 7, 7};
@@ -20,10 +20,14 @@ public class MainStreams {
     }
 
     private static int minValue(int[] values) {
-        return IntStream.of(values).distinct().sorted().reduce((value1, value2) -> value1 * 10 + value2).getAsInt();
+        return IntStream.of(values)
+                .distinct()
+                .sorted()
+                .reduce(0, (value1, value2) -> value1 * 10 + value2);
     }
 
     private static List<Integer> oddOrEven(List<Integer> integers) {
-        return integers.stream().filter(integers.stream().mapToInt(Integer::intValue).sum() % 2 == 0 ? number -> number % 2 == 0 : number -> number % 2 != 0).collect(Collectors.toList());
+        int sum = integers.stream().mapToInt(Integer::intValue).sum();
+        return integers.stream().filter(sum % 2 == 0 ?number -> number % 2 != 0 :number -> number % 2 == 0).collect(Collectors.toList());
     }
 }
