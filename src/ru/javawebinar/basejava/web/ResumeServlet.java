a26e0ad618cdd8e666bda9d857bc06b3ea383164
sashalik <sashal1912@gmail.com>
2019-01-25 20:56:23 +0700

    Jstl, HW15

--- src/ru/javawebinar/basejava/web/ResumeServlet.java
+++ src/ru/javawebinar/basejava/web/ResumeServlet.java
@@ -1,7 +1,6 @@
 package ru.javawebinar.basejava.web;
 
 import ru.javawebinar.basejava.Config;
-import ru.javawebinar.basejava.model.Resume;
 import ru.javawebinar.basejava.storage.Storage;
 
 import javax.servlet.ServletConfig;
@@ -10,7 +9,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.Writer;
 
 public class ResumeServlet extends HttpServlet {
     private Storage storage;
@@ -26,31 +24,7 @@ public class ResumeServlet extends HttpServlet {
     }
 
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws javax.servlet.ServletException, IOException {
-        request.setCharacterEncoding("UTF-8");
-        response.setCharacterEncoding("UTF-8");
-        response.setContentType("text/html; charset=UTF-8");
-        Writer writer = response.getWriter();
-        writer.write(
-                "<html>" +
-                        "<head>" +
-                        "    <title>Список резюме</title>" +
-                        "</head>" +
-                        "<body>" +
-                        "  <center>" +
-                        "  <table border=1 width=640>" +
-                        "    <tr height = 50>" +
-                        "      <td colspan=3> <font size = 5> Список резюме </font> </td>" +
-                        "    </tr>");
-        for (Resume resume : storage.getAllSorted()) {
-            writer.write(
-                    "<tr height = 50>" +
-                            "<td> <center>" + resume.getUuid() + "</center></td>" +
-                            "<td> <center>" + resume.getFullName() + "</center></td>" +
-                            "</tr>");
-        }
-        writer.write("</table>" +
-                "</center>" +
-                "</body>" +
-                "</html>");
+        request.setAttribute("resumes", storage.getAllSorted());
+        request.getRequestDispatcher("/WEB-INF/jsp/list.jsp").forward(request, response);
     }
 }
